dc274036565aaaa25aa3debce950af5bed6a0d06
Arieh Smith <arieh.smith@gmail.com>
2014-07-09 15:21:29 -0400

    Add demo user button

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+u = User.new(username: "Demo")
+u.password = "demopassword"
+u.save!
